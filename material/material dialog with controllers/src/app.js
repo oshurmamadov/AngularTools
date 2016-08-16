bd13c5f8880a9d99b43f26930cd76562d6b3fb2c
oshurmamadov <parviz0492@gmail.com>
2016-08-16 12:06:21 +0530

    Adding controller

---
+++ material/material dialog with controllers/src/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by OshurmamadovP on 12-08-2016.
+ */
+
+angular.module('application',['initModule']);
---
+++ material/material dialog with controllers/src/controllers/dialog_controller.js
@@ -0,0 +1,15 @@
+/**
+ * Created by OshurmamadovP on 12-08-2016.
+ */
+
+
+angular.module('dialogModule',[]).controller("dialog_controller",function($scope,dialog_data_model){
+
+    $scope.dialog = new dialog_data_model();
+    $scope.dialog.title   = "Dialog Title";
+    $scope.dialog.message = "Fill MDL(Material Design Lite) and AngularJS power";
+
+    $scope.showDialog = function () {
+        showDialog('#dialog');
+    };
+});
