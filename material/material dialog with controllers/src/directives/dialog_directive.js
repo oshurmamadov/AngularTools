90d2ca63a883eb4ca220d9ddf93c11dc3f97e3cd
oshurmamadov <parviz0492@gmail.com>
2016-08-17 16:42:11 +0530

    Adding directive

---
+++ material/material dialog with controllers/src/directives/dialog_directive.js
@@ -0,0 +1,14 @@
+/**
+ * Created by OshurmamadovP on 12-08-2016.
+ */
+
+angular.module('initModule')
+    .directive('materialDialog',function () {
+        return{
+            restrict: 'E',
+            scope: {
+                dialogData: '=data'
+            },
+            templateUrl: 'pages/dialog.html'
+        }
+    });
